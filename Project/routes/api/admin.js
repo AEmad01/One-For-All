d127299f8224fec107ddfa1e9f78c4b6345e565b
RagabAhmed <ahmed.ragabmahmoud@student.guc.edu.eg>
2019-03-02 21:12:07 +0200

    RagabFinalCommit

--- Project/routes/api/admin.js
+++ Project/routes/api/admin.js
@@ -5,7 +5,13 @@ const router = express.Router();
 
 // Models
 const Admin = require('../../models/Admin');
-const tasks = require('../../models/task');
+const Task = require('../../models/Task')
+const tasks = [
+    new Task ('10:00', 'high', 3, 'senior' ,'google',4000,'wuzzuf',['active','focused'])
+     ,new Task ('11:00', 'low', 2, 'junior' ,'yahoo',5000,'otlob',['old','focused'] )
+     ,new Task ('9:00', 'medium', 1, 'senior' ,'nike',2000,'wuzzuf',['active','focused','humble'] )
+     ,new Task ('20:00', 'high', 3, 'junior' ,'guc',6000,'sh5al',['humble','focused'] )
+     ,new Task ('22:00', 'low', 1, 'senior' ,'apple',9000,'wuzzuf',['active','productive'] )]
 
 // temporary data created as if it was pulled out of the database ...
 const admins = [
@@ -23,44 +29,6 @@ const admins = [
 // Get all A
 router.get('/', (req, res) => res.json({ data: admins }));
 
-// Create a new Admin
-router.post('/', (req, res) => {
-	const name = req.body.name;
-	const age = req.body.age;
-
-	if (!name) return res.status(400).send({ err: 'Name field is required' });
-	if (typeof name !== 'string') return res.status(400).send({ err: 'Invalid value for name' });
-	if (!age) return res.status(400).send({ err: 'Age field is required' });
-	if (typeof age !== 'number') return res.status(400).send({ err: 'Invalid value for age' });
-
-	const newAdmin = {
-		name,
-		age,
-		id: uuid.v4(),
-	};
-	return res.json({ data: newAdmin });
-});
-
-router.post('/joi', (req, res) => {
-	const name = req.body.name
-	const age = req.body.age
-
-	const schema = {
-		name: Joi.string().min(3).required(),
-		age: Joi.number().required(),
-	}
-
-	const result = Joi.validate(req.body, schema);
-
-	if (result.error) return res.status(400).send({ error: result.error.details[0].message });
-
-	const newAdmin = {
-		name,
-		age,
-		id: uuid.v4(),
-	};
-	return res.json({ data: newAdmin });
-});
 //post a task
 router.post('/',(req,res)=>{
     const time = req.body.time
@@ -79,22 +47,22 @@ router.post('/',(req,res)=>{
     if (typeof effort !== 'string') return res.status(400).send({ err: 'Invalid value for effort' });
 
     if (!level_of_commitment) return res.status(400).send({ err: 'level_of_commitment field is required' });
-    if (typeof level_of_commitment !== 'string') return res.status(400).send({ err: 'Invalid value for level_of_commitment ' });
+    if (typeof level_of_commitment !== 'number') return res.status(400).send({ err: 'Invalid value for level_of_commitment ' });
 
 	if (!the_experience_level) return res.status(400).send({ err: 'the_experience_level field is required' });
-    if (typeof the_experience_level !== 'number') return res.status(400).send({ err: 'Invalid value for the_experience_level' });
+    if (typeof the_experience_level !== 'string') return res.status(400).send({ err: 'Invalid value for the_experience_level' });
 
 	if (!partner_who_owns_it) return res.status(400).send({ err: 'partner_who_owns_it field is required' });
-	if (typeof partner_who_owns_it !== 'number') return res.status(400).send({ err: 'Invalid value for partner_who_owns_it' });
+	if (typeof partner_who_owns_it !== 'string') return res.status(400).send({ err: 'Invalid value for partner_who_owns_it' });
 
 	if (!monetary_compensation) return res.status(400).send({ err: 'monetary_compensation field is required' });
     if (typeof monetary_compensation !== 'number') return res.status(400).send({ err: 'Invalid value for monetary_compensation' });
 
     if (!consultancy_assigned_to_it) return res.status(400).send({ err: 'consultancy_assigned_to_it field is required' });
-    if (typeof consultancy_assigned_to_it !== 'number') return res.status(400).send({ err: 'Invalid value for consultancy_assigned_to_it' });
+    if (typeof consultancy_assigned_to_it !== 'string') return res.status(400).send({ err: 'Invalid value for consultancy_assigned_to_it' });
 
     if (!set_of_skills) return res.status(400).send({ err: 'set_of_skills field is required' });
-    if (typeof set_of_skills !== 'number') return res.status(400).send({ err: 'Invalid value for set_of_skills' });
+    if (typeof set_of_skills !== 'string') return res.status(400).send({ err: 'Invalid value for set_of_skills' });
     
     const newTask = {
 		time,
@@ -106,8 +74,10 @@ router.post('/',(req,res)=>{
         consultancy_assigned_to_it,
         set_of_skills,
 	
-	};
-	return res.json({ data: newTask });
+    };
+    
+    tasks[tasks.length] = newTask;
+	return res.json({ data: tasks });
 
 });
 
@@ -150,7 +120,7 @@ router.post('/joi', (req, res) => {
 	return res.json({ data: newTask });
 });
 //Choose the candidate 
-router.post('/', (req, res) => {
+router.post('/chooseCandidate', (req, res) => {
     const name = req.body.name;
     const taskNumber = req.body.taskNumber;
 
@@ -163,5 +133,13 @@ router.post('/', (req, res) => {
     tasks[taskNumber-1].Candidate = name;
     return res.json({ data: tasks });
 });
+// Delete a task
+router.delete('/api/admin/:id', (req, res) => {
+    const taskid = req.params.id 
+    const task = tasks.find(task => task.id == taskid)
+    const index = tasks.indexOf(task)
+    tasks.splice(index,1)
+    res.send(tasks)
+})
 
 module.exports = router;
