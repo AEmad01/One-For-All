f6ae48eb7ec2b7410a4005775011e7d339167f25
AEmad01 <47536525+AEmad01@users.noreply.github.com>
2019-03-02 19:48:53 +0200

    Update location.js
    Added id to location as a unique identifier for the location.

--- Project/models/location.js
+++ Project/models/location.js
@@ -1,13 +1,16 @@
 // The location model
+const uuid = require('uuid');
 class location {
-    constructor(name, address, city, country, time, sender, reciver) {
+    constructor(name, address, city, country, time, sender, reciever) {
         this.name=name;
         this.address=address;
         this.city=city;
         this.country=country;
         this.time=time;
         this.sender = sender;
-        this.reciver = reciver;
+        this.reciever = reciever;
+        this.id = uuid.v4();
+
     };
 }
 
