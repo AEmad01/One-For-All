d966da51ca729465a1cba61683b8c37480118ffe
abdullahmahmoud4344 <abdullah.abelmagid@student.guc.edu.eg>
2019-03-23 18:12:38 +0200

    validationnedit

--- Project/routes/api/schedules.js
+++ Project/routes/api/schedules.js
@@ -10,7 +10,8 @@ router.get('/', async (req, res) => {
 // Create a new member
 router.post('/', async (req,res) => {
     try {
-        
+    const isValidated = validator.createValidation(req.body)
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })    
     const newSchedule = await Schedule.create(req.body)
      res.json({msg:'Schedule was created successfully', data: newSchedule})
     }
@@ -24,6 +25,9 @@ router.put('/:id', async (req,res) => {
     try {
      const id = req.params.id
      const schedule = await Schedule.find({id})
+     if(!schedule) return res.status(404).send({error: 'Schedule does not exist'})
+     const isValidated = validator.updateValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
     const updatedSchedule = await Schedule.updateOne(req.body)
      res.json({msg: 'Schedule updated successfully'})
     }
