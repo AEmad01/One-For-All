e5385773ac76cdb9f4309451f47c56c6216669ea
hazem9999 <hazem.elhakim@student.guc.edu.eg>
2019-03-23 16:15:15 +0200

    continue Task Validations

--- Project/validations/taskValidations.js
+++ Project/validations/taskValidations.js
@@ -24,14 +24,20 @@ module.exports = {
 
   updateValidation: request => {
     const updateSchema = {
-      name: Joi.string().required(),
-      age: Joi.number().required(),
-      skills: Joi.string().required(),
-      intrests: Joi.string().required(),
-      pastEvents: Joi.string().required(),
-      completedProjects: Joi.string().required(),
-      reviews: Joi.string().required(),
-      certificates: Joi.string().required()
+        name: Joi.string().required(),
+        time: Joi.date().required(),
+        effort: Joi.string().required(),
+        levelOfCommitment: Joi.string().required(),
+        experienceLevel: Joi.string().required(),
+        completedProjects: Joi.string().required(),
+        partnerID: Joi.number().required(),
+        partnerName: Joi.string().required(),
+        monetaryCompensation: Joi.string().required(),
+        consultency: Joi.string().required(),
+        setOfSkills: Joi.array().required(),
+        memberID: Joi.number().required(),
+        memberName: Joi.string().required(),
+        candidates: Joi.array().required()
     };
 
     return Joi.validate(request, updateSchema);
