8019687c33b573c35749995f42f4781050ae23b3
mohamedibrahim07 <mohamed.ahmedmostafa@student.guc.edu.eg>
2019-03-23 22:32:41 +0200

    add a method to update task

--- Project/routes/api/task.js
+++ Project/routes/api/task.js
@@ -21,5 +21,23 @@ router.post("/", async (req, res) => {
     console.log(error);
   }
 });
+// Update a task
+router.put('/:id', async (req,res) => {
+  try {
+   const id = req.params.id
+   const task = await Task.findOne({id})
+   if(!task) return res.status(404).send({error: 'task does not exist'})
+   const isValidated = validator.updateValidation(req.body)
+   if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+   const updatedTask = await Task.updateOne(req.body)
+   res.json({msg: 'Task updated successfully'})
+  }
+  catch(error) {
+      // We will be handling the error later
+      console.log(error)
+  }  
+})
+
+
 
 module.exports = router;
