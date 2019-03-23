2d68781c0ecd480f898037c9db6af5793e15004c
abdullahmahmoud4344 <abdullah.abelmagid@student.guc.edu.eg>
2019-03-23 19:28:25 +0200

    validationedite3

--- Project/routes/api/schedules.js
+++ Project/routes/api/schedules.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 const Schedule = require('../../models/Schedule.js');
+const validator = require('../../validations/scheduleValidations')
 // Get all schedules
 router.get('/', async (req, res) => {
     const schedules = await Schedule.find();
     res.json({ data: schedules })
 });
 
-// Create a new member
+// Create a new schedule
 router.post('/', async (req,res) => {
     try {
     const isValidated = validator.createValidation(req.body)
@@ -38,40 +40,5 @@ router.put('/:id', async (req,res) => {
  })
 
 
-module.exports = router;const express = require('express');
-const router = express.Router();
-const Schedule = require('../../models/Schedule.js');
-// Get all schedules
-router.get('/', async (req, res) => {
-    const schedules = await Schedule.find();
-    res.json({ data: schedules })
-});
-
-// Create a new member
-router.post('/', async (req,res) => {
-    try {
-        
-    const newSchedule = await Schedule.create(req.body)
-     res.json({msg:'Schedule was created successfully', data: newSchedule})
-    }
-    catch(error) {
-        // We will be handling the error later
-        console.log(error)
-    }  
- })
-// Update a Schedule
-router.put('/:id', async (req,res) => {
-    try {
-     const id = req.params.id
-     const schedule = await Schedule.find({id})
-    const updatedSchedule = await Schedule.updateOne(req.body)
-     res.json({msg: 'Schedule updated successfully'})
-    }
-    catch(error) {
-        // We will be handling the error later
-        console.log(error)
-    }  
- })
-
-
 module.exports = router;
+
