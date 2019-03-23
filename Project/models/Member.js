6b271678886f231ca2eb2adf15e670564d49e738
GemsEldeen <ahmed.mohamedhalawa@student.guc.edu.eg>
2019-03-23 16:06:01 +0200

    Prettier

--- Project/models/Member.js
+++ Project/models/Member.js
@@ -1,37 +1,37 @@
 const mongoose= require('mongoose')
 const Schema = mongoose.Schema;
-const MemberSchema= new Schema({
+const MemberSchema = new Schema({
   name: {
     type: String,
     required: true
-},
-age: {
-  type: Number,
-  required: true
-},
-skills: {
-  type: String,
-  required: true
-},
-intrests: {
-  type: String,
-  required: true
-},
-pastEvents: {
-  type: String,
-  required: true
-},
-completedProjects: {
-  type: String,
-  required: true
-},
-reviews: {
-  type: String,
-  required: true
-},
-certificates: {
-  type: String,
-  required: true
-}
-})
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  skills: {
+    type: String,
+    required: true
+  },
+  intrests: {
+    type: String,
+    required: true
+  },
+  pastEvents: {
+    type: String,
+    required: true
+  },
+  completedProjects: {
+    type: String,
+    required: true
+  },
+  reviews: {
+    type: String,
+    required: true
+  },
+  certificates: {
+    type: String,
+    required: true
+  }
+});
   module.exports = Member = mongoose.model('members',MemberSchema);
