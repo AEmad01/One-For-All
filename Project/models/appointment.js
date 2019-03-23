e9c7decc98e4e269de1ea32d7514fb257c91ddf3
omaralex12 <omarmahmoud4898@yahoo.com>
2019-03-23 18:26:04 +0200

    Update Apointment Schema

--- Project/models/appointment.js
+++ Project/models/appointment.js
@@ -1,13 +1,33 @@
-const uuid = require('uuid');
-class appointment{
-    constructor(id,location,slot,lifeCoach,person,confirm){
-        this.id = id;
-        this.location=location;
-        this.slot=slot;
-        this.lifeCoach=lifeCoach;
-        this.person=person;
-        this.confirm = confirm;
-        
-    }
-}
-module.exports=appointment
+const mongoose= require('mongoose')
+const Schema = mongoose.Schema;
+const appointmentSchema = new Schema({
+  location: {
+    type: String,
+    required: true
+  },
+  slot: {
+    type: Date,
+    required: true
+  },
+  lifeCoachID: {
+    type: Number,
+    required: true
+  },
+  lifeCoachName: {
+    type: String,
+    required: true
+  },
+  memberID: {
+    type: Number,
+    required: true
+  },
+  memberName: {
+    type: String,
+    required: true
+  },
+  confirm: {
+    type: boolean,
+    required: true
+  },
+});
+  module.exports = appointment = mongoose.model('appointment',appointmentSchema);
