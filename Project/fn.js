0b11de59d312f152a999b256308d04f1dcac4a5b
GemsEldeen <ahmed.mohamedhalawa@student.guc.edu.eg>
2019-03-31 20:58:54 +0200

    merge bardo

--- Project/fn.js
+++ Project/fn.js
@@ -1,35 +1,5 @@
 const axios = require('axios');
 const functions = {
-<<<<<<< HEAD
-    GetAllSchedule: async (body) =>{
-        const AllSchedule = await axios.get('http://localhost:3000/GetAllSchedule', body)
-        return AllSchedule
-    },
-    GetSpecificSchedule: async (id) =>{
-        const schedule = await axios.get(`http://localhost:3000/GetSpecificSchedule/${id}`)
-        return schedule
-    },
-
-    CreateSchedule: async (body) =>{
-        const schedule = await axios.post('http://localhost:3000/CreateSchedule',body)
-        return schedule
-    },
-    UpdateSchedule: async (id) =>{
-        const schedule = await axios.put(`http://localhost:3000/UpdateSchedule/${id}`)
-        return schedule
-    },
-    DeleteSchedule: async (id) =>{
-        const Allschedule = await axios.delete(`http://localhost:3000/DeleteSchedule/${id}`)
-        return Allschedule
-    },
-    AcceptBooking: async (id) =>{
-        const appointments = await axios.put(`http://localhost:3000/booking/${id}`)
-        return appointments
-    }
-}
-;
-=======
-
         addAtt: async (i,n,d) => await axios.put('http://localhost:3000/api/partners/addAttribute/'+i,{
 
             attributeName: n,
@@ -77,4 +47,3 @@ const functions = {
         },
     }
 module.exports = functions; 
->>>>>>> c315ac0b25123e9872fed3d63403937acad0ad5f
