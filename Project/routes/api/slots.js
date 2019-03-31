b9cee874e63aa3cfd59c10f79a9ea03ee98aa5e4
AEmad01 <47536525+AEmad01@users.noreply.github.com>
2019-03-31 18:08:17 +0200

    Create slots.js

---
+++ Project/routes/api/slots.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+const slot = require('../../models/slot');
+const validator = require('../../validations/slotValidations')
+const Schedule = require('../../models/Schedule.js');
+
+router.get('/getslots', async (req, res) => {
+    const slots = await slot.find();
+    res.json({ data: slots })
+});
+
+router.get('/:id',async (req, res) => {
+    const slotId = req.params.id
+    const slot = await Slot.findById(slotId)  
+    if(!slot) return res.status(400).send({error:result.error.details[0].message});
+    res.send(slot)
+});
+
+
+router.post('/:id', async (req,res) => {
+    const id = req.params.id
+    
+    const isValidated = validator.createValidation(req.body)
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message  })    
+    const schedulem = await Schedule.findById(id);
+    if(schedulem.id!== undefined){
+
+    const newSlot = await slot.create(req.body)
+     res.json({msg:'Slot was created successfully', data: newSlot})
+     schedulem.slot.push(newSlot);
+     const temp = await schedulem.save();
+     res.send(schedulem);
+    }
+
+ });
+
+ router.put('/pick/:id', async (req,res) => {
+    const id = req.params.id
+    try {
+    const picked = await slot.findById(id);
+    if (!picked)
+      return res.status(404).send({ error: "slot does not exist" });
+      const isValidated = validator.updateValidation(req.body);
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message }) ;
+  
+      const updatedslot = await picked.updateOne(req.body);
+       res.json({msg:'slot updated successfully', data: updatedslot})
+    
+      }
+      catch(error) {
+          res.status(404).send({error: 'Only slot can be updated'})
+          console.log(error)
+    }
+    
+
+ });
+
+ module.exports = router;
