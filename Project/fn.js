2a6bcceac5ac0da70322554d57470cd56e877109
Karim Fekry <Karim@KFekry@live.com>
2019-03-31 20:33:23 +0200

    All crud tests for Lifecoach

--- Project/fn.js
+++ Project/fn.js
@@ -34,5 +34,46 @@ const functions = {
             const members = await axios.get('http://localhost:3000/api/members/')
             return members
         },
-    }
+        PostLifecoach: async (body) =>{
+            const PostLifecoach = await axios.post('http://localhost:3000/api/lifecoach/', {
+            "name" :body.name,
+            "age" : body.age,
+            "skills": body.skills,
+            "intrests" : body.intrests,
+            "pastEvents" : body.pastEvents,
+            "completedProjects" : body.completedProjects,
+            "reviews" : body.reviews,
+            "certificates" : body.certificates,
+            "specification" : body.specification,
+            "salary" : body.salary
+            
+            })
+           
+        },
+        GetAllLifecoach: async () =>{
+            const Lifecoach = await axios.get('http://localhost:3000/api/lifecoach/')
+            return Lifecoach
+        },
+        
+        UpdateLifecoach: async (id,body) =>{
+            const Lifecoach = await axios.put('http://localhost:3000/api/lifecoach/'+id, {
+            "name" :body.name,
+            "age" : body.age,
+            "skills": body.skills,
+            "intrests" : body.intrests,
+            "pastEvents" : body.pastEvents,
+            "completedProjects" : body.completedProjects,
+            "reviews" : body.reviews,
+            "certificates" : body.certificates,
+            "specification" : body.specification,
+            "salary" : body.salary
+            
+            })
+           
+        },
+
+        DeleteLifecoach: async (id) =>{
+            const Lifecoach = await axios.delete('http://localhost:3000/api/lifecoach/'+id )
+    },
+}
 module.exports = functions; 
--- Project/routes/api/lifecoach.js
+++ Project/routes/api/lifecoach.js
@@ -58,39 +58,33 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const lifecoach = await Lifecoach.find({ id });
-    if (!lifecoach)
-      return res.status(404).send({ error: "life coach does not exist" });
+    const lifecoach = await Lifecoach.find({id});
+    if (!lifecoach) return res.status(404).send({ error: "lifecoach does not exist" });
     const isValidated = validator.updateValidation(req.body);
     if (isValidated.error)
       return res
         .status(400)
         .send({ error: isValidated.error.details[0].message });
-    const updatedlifecoach = await Lifecoach.updateOne(req.body);
-    res.json({
-      msg: "life coach updated successfully",
-      data: updatedlifecoach
-    });
+    
+    const updatedLifecoach = await Lifecoach.findOneAndUpdate({_id: id} , req.body);
+    res.json({ msg: "Lifecoach updated successfully" });
   } catch (error) {
     // We will be handling the error later
     console.log(error);
   }
 });
 // delete a certain lifecoach
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req,res) => {
   try {
-    const id = req.params.id;
-    const lifecoach = await Lifecoach.find({ id });
-    const life = lifecoach._id;
-    const deletedlifecoach = await Lifecoach.findOneAndDelete(life);
-    res.json({
-      msg: "life coach was deleted successfully",
-      data: deletedlifecoach
-    });
-  } catch (error) {
-    // We will be handling the error later
-    console.log(error);
+   const id = req.params.id
+   const deletedLifecoach = await Lifecoach.findByIdAndDelete(id);
+   
+   res.json({msg:'Lifecoach was deleted successfully', data: deletedLifecoach})
   }
+  catch(error) {
+      // We will be handling the error later
+      console.log(error)
+  }  
 });
 
 module.exports = router;
