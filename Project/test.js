7af82ea213ec4edabe04c98e995ac81fd7ba04d6
YoussefMedhat <youssefmedhat732@gmail.com>
2019-03-31 19:42:26 +0200

    updated tests

--- Project/test.js
+++ Project/test.js
@@ -2,8 +2,8 @@ const funcs = require('./fn')
 
 test('adds an extra attribute to a task', async () =>{
     
-    const taskID = '5c9cdf21c2343413bf99fcfc'
-    const desc = 'asdsgsagsagas'
+    const taskID = '5ca0ec29e6d969466c46796d'
+    const desc = 'czsdxfcgvhbd'
     const name = 'asga'
     expect.assertions(1)
     await funcs.addAtt(taskID,name,desc)    
@@ -15,9 +15,9 @@ test('adds an extra attribute to a task', async () =>{
 
 test('choosing a applicant to be assigned to a task', async () => {
 
-    const memberID = '5c9cceb2a2f2670e9fee71a3'
-    const taskID = '5c9cdf21c2343413bf99fcfc'
-    const name = 'asdasdasd'
+    const memberID = '5ca0e47db3f32c0de8a8c75c'
+    const taskID = '5ca0ec29e6d969466c46796d'
+    const name = 'Karim'
     await funcs.chooseApp(memberID,taskID,name)
     expect.assertions(2)
     const response = await funcs.getTasks()
