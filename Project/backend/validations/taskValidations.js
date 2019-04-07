8c18c3b444566f22e6ccb181f96ba9984e7de975
mohamedibrahim07 <mohamed.ahmedmostafa@student.guc.edu.eg>
2019-04-07 21:58:17 +0200

    update task react

--- Project/backend/validations/taskValidations.js
+++ Project/backend/validations/taskValidations.js
@@ -25,22 +25,22 @@ module.exports = {
 
   updateValidation: request => {
     const updateSchema = {
-        name: Joi.string().required(),
-        time: Joi.date().required(),
-        effort: Joi.string().required(),
-        levelOfCommitment: Joi.string().required(),
-        experienceLevel: Joi.string().required(),
-        partnerID: Joi.number().required(),
-        partnerName: Joi.string().required(),
-        monetaryCompensation: Joi.string().required(),
-        consultency: Joi.string().required(),
-        setOfSkills: Joi.array().required(),
-        memberID: Joi.number().required(),
-        memberName: Joi.string().required(),
-        candidates: Joi.array().required(),
-        negotiation:Joi.string().required(),
-        Description: Joi.string().required(),
-        extraAtt:Joi.array().required()
+        name: Joi.string(),
+        time: Joi.date(),
+        effort: Joi.string(),
+        levelOfCommitment: Joi.string(),
+        experienceLevel: Joi.string(),
+        partnerID: Joi.number(),
+        partnerName: Joi.string(),
+        monetaryCompensation: Joi.string(),
+        consultency: Joi.string(),
+        setOfSkills: Joi.array(),
+        memberID: Joi.number(),
+        memberName: Joi.string(),
+        candidates: Joi.array(),
+        negotiation:Joi.string(),
+        Description: Joi.string(),
+        extraAtt:Joi.array()
         
     };
 
--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -14,7 +14,7 @@ import LocationList from './components/locationList'
 import updateLocation from './components/updateLocation'
 import AppointmentList from './components/appointment-list'
 import BookAppointment from './components/book-appointment'
-
+import UpdateTask from './components/updatetask'
 class App extends Component {
   render() {
     return (  
@@ -58,7 +58,7 @@ class App extends Component {
           <Route path='/partners' exact component={PartnerList} />
           <Route path='/partners/createPartner' exact component={CreatePartner} />
           <Route path='/slots' exact component={Slots} />
-
+          <Route path='/update/:id' component={UpdateTask} />
           <Route path='/locations' exact component={LocationList} />
           <Route path='/put/:id' component={updateLocation} />
   <Route path='/appointments' exact component={AppointmentList} />
