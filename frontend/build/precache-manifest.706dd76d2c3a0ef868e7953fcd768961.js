672b81751b42a7295c6adf9fab2ecf94513bddea
YoussefMedhat <youssefmedhat732@gmail.com>
2019-04-15 15:54:52 +0200

    t

---
+++ frontend/build/precache-manifest.706dd76d2c3a0ef868e7953fcd768961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d799137216c1f52c2b6e",
+    "url": "/static/js/main.1795efc1.chunk.js"
+  },
+  {
+    "revision": "cb9eddef1ba2be31be10",
+    "url": "/static/js/2.1a7bc9c8.chunk.js"
+  },
+  {
+    "revision": "d799137216c1f52c2b6e",
+    "url": "/static/css/main.b4db4faa.chunk.css"
+  },
+  {
+    "revision": "cb9eddef1ba2be31be10",
+    "url": "/static/css/2.82bb7bd3.chunk.css"
+  },
+  {
+    "revision": "17f21d6910c3f8060a407a7281b91852",
+    "url": "/index.html"
+  }
+];
